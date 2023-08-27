7f99cec6d776448afd7bf595102637459a472682
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2023-08-27 17:38:31 +0530

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,30 @@
-import express from 'express';
-import http from 'http';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import compression from 'compression';
-import mongoose from 'mongoose';
-import router from './router';
+import express from "express";
+import http from "http";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+import mongoose from "mongoose";
+import router from "./router";
 
-const PORT = 8000
+const PORT = 8000;
 const app = express();
-app.use(cors({
-    credentials:true,
-}));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(compression());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/',router())
+app.use("/", router());
 
 const server = http.createServer(app);
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/typescipt-1'
+const MONGO_URL =
+  "mongodb+srv://srivastavaaryanalc76:L2lxZIEj7Z16ppXV@cluster0.3gdjlph.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 
-server.listen(PORT,()=>console.log('Server started on PORT ' + PORT))
+server.listen(PORT, () => console.log("Server started on PORT " + PORT));
